1090624afdc49b4de01a1bfa896e1e2dbe6e0505
grace_goose <choieungi@gm.gist.ac.kr>
2021-05-22 14:44:45 +0900

    direct_spring_bean_register

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,24 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// MemberService와 MemberRepository의 @Service, @Repository, @Autowired annotation을 제거
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+
+}
